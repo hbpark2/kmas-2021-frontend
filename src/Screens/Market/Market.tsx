bd626c30fc4013c3c2eec99398ebe64a65de1c1f
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-11-25 11:26:26 +0900

    form WIP

--- src/Screens/Market/Market.tsx
+++ src/Screens/Market/Market.tsx
@@ -1,13 +1,14 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import DaumPostcode from "react-daum-postcode";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import styled from "styled-components";
 import Modal from "../../Components/Common/Modal";
 import { useState } from "react";
-import { FormValues } from "./marketType";
+import { categoryArr, FormValues, inputArr, schema } from "./marketType";
 import Utils from "../../Utils/Utils";
 import { getBase64Format } from "../../Utils/base64";
-import { getByDisplayValue } from "@testing-library/dom";
 
 const Container = styled.main``;
 const Form = styled.form`
@@ -35,13 +36,11 @@ const Market = () => {
 		register,
 		handleSubmit,
 		formState: { errors },
-		reset,
-		watch,
 		setValue,
 		trigger,
-		control,
 	} = useForm<FormValues | any>({
 		// mode: "onChange",
+		resolver: yupResolver(schema),
 	});
 
 	const [isPostOpen, setIsPostOpen] = useState(false);
@@ -83,19 +82,6 @@ const Market = () => {
 
 	console.log(errors);
 
-	const categoryArr = [
-		{ text: "전체", value: 0 },
-		{ text: "생활/가구/주방", value: 1 },
-		{ text: "뷰티", value: 2 },
-		{ text: "식품", value: 3 },
-		{ text: "가전/디지털", value: 4 },
-		{ text: "스포츠/레저/자동차", value: 5 },
-		{ text: "패션/잡화", value: 6 },
-		{ text: "문구/오피스", value: 7 },
-		{ text: "출산/유아동", value: 8 },
-		{ text: "기타", value: 9 },
-	];
-
 	const onPostComplete = (data: any) => {
 		const { zonecode, jibunAddress, autoJibunAddress, address, roadAddress } = data;
 		setValue("zonecode", zonecode);
@@ -104,33 +90,6 @@ const Market = () => {
 		trigger("road_address");
 		setIsPostOpen(false);
 	};
-
-	const inputArr = [
-		{
-			text: "이름",
-			name: "name",
-			type: "text",
-			required: true,
-			placeholder: "필수 입력항목입니다.",
-			validate: (value?: string) => (value && value.length < 4 ? "3자 이상" : true),
-		},
-		{
-			text: "홈페이지",
-			name: "hompage_link",
-			type: "text",
-			required: true,
-			placeholder: "필수 입력항목입니다.",
-			validate: (value?: string) => (value && value.length < 4 ? "3자 이상" : true),
-		},
-		{
-			text: "핸드폰 번호",
-			name: "phone_number",
-			type: "text",
-			required: true,
-			placeholder: "필수 입력항목입니다.",
-			validate: (value?: string) => (value && value.length < 4 ? "3자 이상" : true),
-		},
-	];
 
 	return (
 		<Container>
@@ -144,209 +103,104 @@ const Market = () => {
 				</select>
 
 				{inputArr.map((item, index) => {
-					console.log(errors);
+					if (item.text === "주소") {
+						return (
+							<label htmlFor="detail_address" key={`label${index}`}>
+								<span>주소</span>
+								<div>
+									<input
+										type="text"
+										id="zonecode"
+										readOnly={true}
+										{...register("zonecode", {
+											required: true,
+											validate: (value?: string) =>
+												value && value.length < 4 ? "정확히 입력해주세요." : true,
+										})}
+										placeholder="필수입력항목입니다."
+									/>
+									<button
+										onClick={(e) => {
+											e.preventDefault();
+											setIsPostOpen(true);
+										}}
+										type="button"
+									>
+										우편번호 찾기
+									</button>
+								</div>
+								<div>
+									<input
+										type="text"
+										id="jibun_address"
+										{...register("jibun_address", {
+											required: true,
+											validate: (value?: string) =>
+												value && value.length < 4 ? "정확히 입력해주세요." : true,
+										})}
+										placeholder="필수입력항목입니다."
+									/>
+								</div>
+								<div>
+									<input
+										type="text"
+										id="road_address"
+										{...register("road_address", {
+											required: true,
+											validate: (value?: string) =>
+												value && value.length < 4 ? "정확히 입력해주세요." : true,
+										})}
+										placeholder="필수입력항목입니다."
+									/>
+								</div>
 
-					return (
-						<label htmlFor={item.name} key={`label${index}`}>
-							<span>{item.text}</span>
-							<input
-								type={item.type}
-								id={item.name}
-								{...register(item.name, {
-									required: true,
-									validate: (value) => item.validate(value),
-								})}
-								placeholder={item.placeholder}
-							/>
+								<div>
+									<input
+										type="text"
+										id="detail_address"
+										{...register("detail_address", {
+											required: true,
+											validate: (value?: string) =>
+												value && value.length < 4 ? "정확히 입력해주세요." : true,
+										})}
+										placeholder="상세주소"
+									/>
+								</div>
+							</label>
+						);
+					} else {
+						return (
+							<label htmlFor={item.name} key={`label${index}`}>
+								<span>{item.text}</span>
+								<input
+									type={item.type}
+									id={item.name}
+									{...register(item.name)}
+									placeholder={item.placeholder}
+								/>
 
-							{item.name === "name" && errors.name && <p>{`${errors.name.message}`}</p>}
-
-							{item.name === "hompage_link" && errors.hompage_link && (
-								<p>{`${errors.hompage_link.message}`}</p>
-							)}
-
-							{item.name && "phone_number" && errors.phone_number && (
-								<p>{`${errors.phone_number.message}`}</p>
-							)}
-						</label>
-					);
+								{(item.name === "name" && errors.name && <p>{`${errors.name.message}`}</p>) ||
+									(item.name === "hompage_link" && errors.hompage_link && (
+										<p>{`${errors.hompage_link.message}`}</p>
+									)) ||
+									(item.name && "phone_number" && errors.phone_number && (
+										<p>{`${errors.phone_number.message}`}</p>
+									)) ||
+									(item.name && "password" && errors.password && (
+										<p>{`${errors.password.message}`}</p>
+									)) ||
+									(item.name && "crn" && errors.crn && <p>{`${errors.crn.message}`}</p>) ||
+									(item.name && "items" && errors.items && <p>{`${errors.items.message}`}</p>) ||
+									(item.name && "promotion" && errors.promotion && (
+										<p>{`${errors.promotion.message}`}</p>
+									)) ||
+									(item.name && "exhibition_link" && errors.exhibition_link && (
+										<p>{`${errors.exhibition_link.message}`}</p>
+									))}
+							</label>
+						);
+					}
 				})}
-
-				{/* <label htmlFor="name">
-					<span>이름</span>
-					<input
-						type="text"
-						id="name"
-						{...register("name", {
-							required: true,
-							validate: (value?: string) => (value && value.length < 4 ? "3자 이상" : true),
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-
-					{errors.name && <p>{`${errors.name?.message}`}</p>}
-				</label>
-
-				<label htmlFor="hompage_link">
-					<span>홈페이지</span>
-					<input
-						type="text"
-						id="hompage_link"
-						{...register("hompage_link", {
-							required: true,
-							validate: (value?: string) => (value && value.length < 4 ? "3자 이상" : true),
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-
-					{errors.hompage_link && <p>{`${errors.hompage_link?.message}`}</p>}
-				</label>
-
-				<label htmlFor="phone_number">
-					<span>핸드폰 번호</span>
-					<input
-						type="text"
-						id="phone_number"
-						{...register("phone_number", {
-							required: true,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-					{errors.phone_number && <p>{`${errors.phone_number?.message}`}</p>}
-				</label> */}
-
-				<label htmlFor="password">
-					<span>비밀 번호</span>
-					<input
-						type="password"
-						id="password"
-						{...register("password", {
-							required: true,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-						autoComplete="on"
-					/>
-					{errors.password && <p>{`${errors.password?.message}`}</p>}
-				</label>
-
-				<label htmlFor="crn">
-					<span>사업자 번호</span>
-					<input
-						type="text"
-						id="crn"
-						{...register("crn", {
-							required: true,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-					{errors.crn && <p>{`${errors.crn?.message}`}</p>}
-				</label>
-
-				<label htmlFor="detail_address">
-					<span>주소</span>
-					<div>
-						<input
-							type="text"
-							id="zonecode"
-							readOnly={true}
-							{...register("zonecode", {
-								required: true,
-								validate: (value?: string) =>
-									value && value.length < 4 ? "정확히 입력해주세요." : true,
-							})}
-							placeholder="필수입력항목입니다."
-						/>
-						<button onClick={() => setIsPostOpen(true)}>우편번호 찾기</button>
-					</div>
-					<div>
-						<input
-							type="text"
-							id="jibun_address"
-							{...register("jibun_address", {
-								required: true,
-								validate: (value?: string) =>
-									value && value.length < 4 ? "정확히 입력해주세요." : true,
-							})}
-							placeholder="필수입력항목입니다."
-						/>
-					</div>
-					<div>
-						<input
-							type="text"
-							id="road_address"
-							{...register("road_address", {
-								required: true,
-								validate: (value?: string) =>
-									value && value.length < 4 ? "정확히 입력해주세요." : true,
-							})}
-							placeholder="필수입력항목입니다."
-						/>
-					</div>
-
-					<div>
-						<input
-							type="text"
-							id="detail_address"
-							{...register("detail_address", {
-								required: true,
-								validate: (value?: string) =>
-									value && value.length < 4 ? "정확히 입력해주세요." : true,
-							})}
-							placeholder="상세주소"
-						/>
-					</div>
-				</label>
-
-				<label htmlFor="items">
-					<span>주요 판매품목</span>
-					<input
-						type="text"
-						id="items"
-						{...register("items", {
-							required: true,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-					{errors.items && <p>{`${errors.items?.message}`}</p>}
-				</label>
-
-				<label htmlFor="promotion">
-					<span>프로모션 내용</span>
-					<input
-						type="text"
-						id="promotion"
-						{...register("promotion", {
-							required: true,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-					{errors.promotion && <p>{`${errors.promotion?.message}`}</p>}
-				</label>
-
-				<label htmlFor="exhibition_link">
-					<span>기획전 링크</span>
-					<input
-						type="text"
-						id="exhibition_link"
-						{...register("exhibition_link", {
-							required: false,
-							validate: (value?: string) =>
-								value && value.length < 4 ? "정확히 입력해주세요." : true,
-						})}
-						placeholder="필수입력항목입니다."
-					/>
-					{errors.exhibition_link && <p>{`${errors.exhibition_link?.message}`}</p>}
-				</label>
 
 				{preview && (
 					<div>
@@ -386,7 +240,7 @@ const Market = () => {
 					</Modal>
 				)}
 
-				<button onClick={handleSubmit(onSubmit)}>제출</button>
+				<input type="submit"></input>
 			</Form>
 		</Container>
 	);
--- src/Screens/Market/marketType.ts
+++ src/Screens/Market/marketType.ts
@@ -1,3 +1,73 @@
+import * as yup from "yup";
+
+export const inputArr = [
+	{
+		text: "이름",
+		name: "name",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "홈페이지",
+		name: "hompage_link",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "핸드폰 번호",
+		name: "phone_number",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "비밀번호",
+		name: "password",
+		type: "password",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "사업자 번호",
+		name: "crn",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "주소",
+		name: "address",
+	},
+	{
+		text: "주요 판매품목",
+		name: "items",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "프로모션 내용",
+		name: "promotion",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+	{
+		text: "기획전 링크",
+		name: "exhibition_link",
+		type: "text",
+		placeholder: "필수 입력항목입니다.",
+	},
+];
+
+export const categoryArr = [
+	{ text: "전체", value: 0 },
+	{ text: "생활/가구/주방", value: 1 },
+	{ text: "뷰티", value: 2 },
+	{ text: "식품", value: 3 },
+	{ text: "가전/디지털", value: 4 },
+	{ text: "스포츠/레저/자동차", value: 5 },
+	{ text: "패션/잡화", value: 6 },
+	{ text: "문구/오피스", value: 7 },
+	{ text: "출산/유아동", value: 8 },
+	{ text: "기타", value: 9 },
+];
+
 export type FormValues = {
 	category?: string;
 	name?: string | any;
@@ -14,3 +84,24 @@ export type FormValues = {
 	exhibition_link?: string;
 	image?: File;
 };
+
+const REQUIRED_TEXT = "필수 항목입니다.";
+
+export const schema = yup
+	.object()
+	.shape({
+		category: yup.string().required(REQUIRED_TEXT),
+		name: yup.string().min(5, "5자 이상").required(REQUIRED_TEXT),
+		hompage_link: yup.string().required(REQUIRED_TEXT),
+		phone_number: yup.string().required(REQUIRED_TEXT),
+		password: yup.string().required(REQUIRED_TEXT),
+		crn: yup.string().required(REQUIRED_TEXT),
+		zonecode: yup.string().required(REQUIRED_TEXT),
+		jibun_address: yup.string().required(REQUIRED_TEXT),
+		road_address: yup.string().required(REQUIRED_TEXT),
+		detail_address: yup.string().required(REQUIRED_TEXT),
+		items: yup.string().required(REQUIRED_TEXT),
+		promotion: yup.string().required(REQUIRED_TEXT),
+		image: yup.string().required(REQUIRED_TEXT),
+	})
+	.required();
