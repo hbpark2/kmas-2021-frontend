b14a6aa1d1e93bed08a7bd3b4f1725e0564762b2
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-12-13 19:38:36 +0900

    video

--- src/Screens/Home/components/SwiperSection.tsx
+++ src/Screens/Home/components/SwiperSection.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
 import SwiperCore, {
@@ -10,14 +10,19 @@ import SwiperCore, {
 } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
+import { useState } from "react";
 SwiperCore.use([Navigation, Pagination, Autoplay, Keyboard, Mousewheel]);
 
-export const SwiperWrap = styled.div`
-  margin: 0 auto;
-  padding-bottom: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const SwiperWrap = styled.div<{ buttonCurrent?: boolean }>`
+  ${({ buttonCurrent }) =>
+    buttonCurrent
+      ? css`
+          margin: 0 auto 50px;
+        `
+      : css`
+          margin: 0 auto 200px;
+        `};
+
   .swiper-button-next,
   .swiper-button-prev {
     color: ${({ theme: { headerDefault } }) => headerDefault};
@@ -28,20 +33,16 @@ export const SwiperWrap = styled.div`
   .swiper-button-prev {
     margin-left: 65px;
   }
+  .videoBox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   .swiper-pagination-bullet {
     background-color: ${({ theme: { headerDefault } }) => headerDefault};
   }
-  iframe {
-    margin: 0 auto;
-    width: 1220px;
-    height: 770px;
-  }
-
   @media screen and (min-width: 1600px) {
     width: 1600px;
-    iframe {
-      width: 85% !important;
-    }
   }
 
   @media screen and (max-width: 1279px) {
@@ -55,25 +56,26 @@ export const SwiperWrap = styled.div`
     }
     .swiper-button-next {
       margin-right: 20px;
+      &::after {
+        font-size: 50px;
+      }
     }
     .swiper-button-prev {
       margin-left: 20px;
-    }
-
-    .swiper-button-next {
       &::after {
-        font-size: 3.2rem;
-      }
-    }
-    .swiper-button-prev {
-      &::after {
-        font-size: 3.2rem;
+        font-size: 50px;
       }
     }
   }
-
   @media screen and (max-width: 767px) {
-    padding-bottom: 50px;
+    ${({ buttonCurrent }) =>
+      buttonCurrent
+        ? css`
+            margin: 0 auto;
+          `
+        : css`
+            margin: 0 auto 100px;
+          `};
 
     iframe {
       width: 85% !important;
@@ -81,9 +83,15 @@ export const SwiperWrap = styled.div`
     }
     .swiper-button-next {
       margin-right: -5px;
+      &::after {
+        font-size: 22px;
+      }
     }
     .swiper-button-prev {
       margin-left: -5px;
+      &::after {
+        font-size: 22px;
+      }
     }
     .swiper-pagination {
       span {
@@ -98,52 +106,132 @@ export const EventLinkButton = styled(Link)`
   display: block;
   margin: 0 auto;
   max-width: 700px;
-  margin: 0 auto;
+  margin: 20px auto 0;
   width: 80%;
   img {
-    width: 100%;
+    display: block;
+    width: 80%;
+    margin: 0 auto;
+  }
+  @media screen and (max-width: 767px) {
+    margin-top: 10px;
   }
 `;
 
 const SwiperSection = () => {
+  const [buttonCurrent, setButtonCurrent] = useState(false);
   return (
-    <SwiperWrap>
-      <h3 className="blind">K-MAS CF영상</h3>
+    <>
+      <SwiperWrap buttonCurrent={buttonCurrent}>
+        <h3 className="blind">K-MAS CF영상</h3>
+        <Swiper
+          navigation
+          spaceBetween={0}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide>
+            {({ isActive }) =>
+              isActive && (
+                <div className="videoBox">
+                  <iframe
+                    width={window.innerWidth > 1280 ? "1200px" : "80%"}
+                    height={window.innerWidth > 1280 ? "675px" : "80%"}
+                    src="https://www.youtube.com/embed/PQ7urgHCm4E?mute=1&autoplay=1&playlist=PQ7urgHCm4E&loop=1"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                </div>
+              )
+            }
+          </SwiperSlide>
+          <SwiperSlide>
+            {({ isActive }) => {
+              setButtonCurrent(isActive);
+              return (
+                isActive && (
+                  <div className="videoBox">
+                    <iframe
+                      width={window.innerWidth > 1280 ? "1200px" : "80%"}
+                      height={window.innerWidth > 1280 ? "675px" : "80%"}
+                      src="https://www.youtube.com/embed/XZbyn7j5Hc4?mute=1&autoplay=1&playlist=XZbyn7j5Hc4&loop=1"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    />
+                  </div>
+                )
+              );
+            }}
+          </SwiperSlide>
+        </Swiper>
+        {/* <Swiper
+          navigation
+          spaceBetween={0}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/PQ7urgHCm4E?mute=1&autoplay=1&playlist=PQ7urgHCm4E&loop=1"
+              title="YouTube video player"
+              width={window.innerWidth > 1280 ? "1200px" : "80%"}
+              height={window.innerWidth > 1280 ? "675px" : "80%"}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {({ isActive }) => {
+              setButtonCurrent(isActive);
+              return (
+                isActive && (
+                  <iframe
+                    src="https://www.youtube.com/embed/PQ7urgHCm4E?mute=1&autoplay=1&playlist=PQ7urgHCm4E&loop=1"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                )
+              );
+            }}
+          </SwiperSlide>
+        </Swiper> */}
 
-      {/* <Swiper navigation spaceBetween={0} slidesPerView={1} pagination={{ clickable: true }}>
-						<SwiperSlide
-							style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-						>
-							{({ isActive }) =>
-								isActive && (
-									<iframe
-										width={window.innerWidth > 1280 ? "1200px" : "80%"}
-										height={window.innerWidth > 1280 ? "675px" : "80%"}
-										src="https://www.youtube.com/embed/PQ7urgHCm4E?mute=1&autoplay=1&playlist=PQ7urgHCm4E&loop=1"
-										title="YouTube video player"
-										frameBorder="0"
-										allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-										allowFullScreen
-									/>
-								)
-							}
-						</SwiperSlide>
-					</Swiper> */}
-      <iframe
+        {/* <iframe
         src="https://www.youtube.com/embed/PQ7urgHCm4E?mute=1&autoplay=1&playlist=PQ7urgHCm4E&loop=1"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      />
-
-      {/* <EventLinkButton to="/">
-					<img
-						src="https://thegn.speedgabia.com/kmas-2021/main/video-event-btn.png"
-						alt="이벤트바로가기버튼"
-					/>
-				</EventLinkButton> */}
-    </SwiperWrap>
+      /> */}
+      </SwiperWrap>
+      {buttonCurrent && (
+        <EventLinkButton to="/event/quiz">
+          <img
+            src="https://thegn.speedgabia.com/kmas-2021/main/video-event-btn.png"
+            alt="이벤트바로가기버튼"
+          />
+        </EventLinkButton>
+      )}
+    </>
   );
 };
 
