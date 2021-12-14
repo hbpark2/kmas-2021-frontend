3e2371044fa793faeb61bf640c9e293185e87cb2
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-12-14 13:50:36 +0900

    index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,4 @@
-import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./Routes/App";
-import { StoreProvider } from "./Context/ContextStore";
 
-const rootElement = document.getElementById("root")! as HTMLElement;
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <StoreProvider>
-      <App />
-    </StoreProvider>,
-    rootElement
-  );
-} else {
-  render(
-    <StoreProvider>
-      <App />
-    </StoreProvider>,
-    rootElement
-  );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
