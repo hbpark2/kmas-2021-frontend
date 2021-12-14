b14060d950011fb864ea7cadb10e8f91de99553c
baekjung <zvgandam@gmail.com>
2021-12-14 11:13:16 +0900

    og

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,18 @@
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
+const rewireStyledComponents = require("create-react-app-rewire-styled-components");
 const path = require("path");
 
 module.exports = (config, env) => {
-  if (env === "production") {
-    config.plugins = config.plugins.concat([
-      new PrerenderSPAPlugin({
-        routes: ["/event/quiz"],
-        staticDir: path.join(__dirname, "build"),
-      }),
-    ]);
-  }
+  // if (env === "production") {
+  config.plugins = config.plugins.concat([
+    new PrerenderSPAPlugin({
+      routes: ["/event/quiz"],
+      staticDir: path.join(__dirname, "build"),
+    }),
+  ]);
+  // }
+
+  config = rewireStyledComponents(config, env);
 
   return config;
 };
